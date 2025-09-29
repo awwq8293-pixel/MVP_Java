a80cf59ac804d9262d2eca8e132ffa48658dc619
awwq8293-pixel <awwq8293@gmail.com>
2025-09-28 22:22:41 -0500

    agregando el main para q funcione

--- MVP/src/MainMVP/Main.java
+++ MVP/src/MainMVP/Main.java
@@ -1,11 +1,11 @@
+import views.ComboView;
 
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        new ComboView();
+        
     }
     
 }
